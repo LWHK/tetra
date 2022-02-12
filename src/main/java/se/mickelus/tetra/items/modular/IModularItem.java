ab970b85895c272c6cfca772192f0ef3958b30c1
WuzgXY-GitHub <wuzgxy@126.com>
2022-02-12 17:35:12 +0800

    1

--- src/main/java/se/mickelus/tetra/items/modular/IModularItem.java
+++ src/main/java/se/mickelus/tetra/items/modular/IModularItem.java
@@ -911,7 +911,7 @@ public interface IModularItem {
     }
 
     default String getDisplayNamePrefixes(ItemStack itemStack) {
-        Stream<String> abcdefg = Stream.concat(
+        return Stream.concat(
                 Arrays.stream(getImprovements(itemStack))
                         .map(improvement -> improvement.key + ".prefix")
                         .filter(I18n::hasKey)
@@ -921,8 +921,8 @@ public interface IModularItem {
                         .map(module -> module.getItemPrefix(itemStack))
                         .filter(Objects::nonNull)
         )
-                .limit(2);
-       return abcdefg.reduce("", (result, prefix) -> result + prefix + " ");
+                .limit(2)
+                .reduce("", (result, prefix) -> result + prefix + " ");
     }
 
     default String getItemName(ItemStack itemStack) {
